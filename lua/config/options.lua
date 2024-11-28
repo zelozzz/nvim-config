6e1abc82e63d1280563cae3734cd32f085ac907f
zelo <1172435011@qq.com>
2024-11-28 16:09:15 +0800

    设置tab size 8个空格

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- local opt = vim.opt
+local opt = vim.opt
 -- opt.relativenumber = false
+
+opt.tabstop = 8 -- Number of spaces tabs count for
